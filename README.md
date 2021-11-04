9d78a4b98d229b13f3f278a2fcf2f48d08059b8c
ong-wei-hong <ongweihong@protonmail.com>
2021-11-04 12:53:23 +0800

    completed mini message board

---
+++ README.md
@@ -0,0 +1,18 @@
+# Mini Message Board
+<p><a href="https://www.theodinproject.com/paths/full-stack-javascript/courses/nodejs/lessons/mini-message-board">link</a> to Project: Mini Message Board</p>
+
+<p>A message board where users can post messages.</p>
+
+## Installation
+<p>Requires</p>
+<ul>
+	<li>npm</li>
+</ul>
+
+<p>To run:</p>
+<p><code>npm install</code> in the root directory</p>
+<p>then, <code>npm start</code></p>
+<p>go to <a href='localhost:3000'>localhost:3000</a></p>
+
+## Note
+No database is used and the message is appended to the end of an initialised js array. So the message will disappear upon refresh.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date()
+	},
+	{
+		text: 'Hello world!',
+		user: 'Charles',
+		added: new Date()
+	}
+]
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', (req, res, next) => {
+	messages.unshift({
+		text: req.body.message,
+		user: req.body.name,
+		added: new Date()
+	})
+	res.redirect('/')
 });
 
 module.exports = router;
